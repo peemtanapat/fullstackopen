ceb3c0292d002fb0fbfa66075ddbe434c66daa6d
peemtanapat <pm.tanapat@gmail.com>
2023-12-07 22:04:19 +0700

    Refactor Total return

--- part1/courseinfo/src/components/Total.jsx
+++ part1/courseinfo/src/components/Total.jsx
@@ -1,16 +1,10 @@
-import { Fragment } from 'react';
-
 const Total = ({ parts }) => {
   const numberOfExercises = parts.reduce(
     (prev, curr) => curr.exercises + prev,
     0
   );
 
-  return (
-    <Fragment>
-      <p>Number of exercises {numberOfExercises}</p>
-    </Fragment>
-  );
+  return <p>Number of exercises {numberOfExercises}</p>;
 };
 
 export default Total;
