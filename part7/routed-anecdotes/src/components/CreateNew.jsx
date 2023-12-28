a148e4b53538b090e01c8968da73f08ff34cafef
peemtanapat <pm.tanapat@gmail.com>
2023-12-28 15:45:22 +0700

    Apply useField (7.4)

--- part7/routed-anecdotes/src/components/CreateNew.jsx
+++ part7/routed-anecdotes/src/components/CreateNew.jsx
@@ -1,21 +1,21 @@
-import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { useField } from '../hooks'
 
 const CreateNew = ({ addNew, setNotification }) => {
   const navigate = useNavigate()
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
+  const content = useField('text')
+  const author = useField('text')
+  const info = useField('text')
 
   const handleSubmit = (e) => {
     e.preventDefault()
     addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0,
     })
-    setNotification(`Added '${content}'`)
+    setNotification(`Added '${content.value}'`)
     navigate('/anecdotes')
   }
 
@@ -27,25 +27,21 @@ const CreateNew = ({ addNew, setNotification }) => {
           <label htmlFor="content">content </label>
           <input
             name="content"
-            value={content}
-            onChange={(e) => setContent(e.target.value)}
+            value={content.value}
+            onChange={content.onChange}
           />
         </div>
         <div>
           <label htmlFor="author">author </label>
           <input
             name="author"
-            value={author}
-            onChange={(e) => setAuthor(e.target.value)}
+            value={author.value}
+            onChange={author.onChange}
           />
         </div>
         <div>
           <label htmlFor="info">url for more info </label>
-          <input
-            name="info"
-            value={info}
-            onChange={(e) => setInfo(e.target.value)}
-          />
+          <input name="info" value={info.value} onChange={info.onChange} />
         </div>
         <button>create</button>
       </form>
