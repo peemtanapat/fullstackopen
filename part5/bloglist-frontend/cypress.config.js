51deb138b11207cc920d7b0324d53d9c4b06a0b9
peemtanapat <pm.tanapat@gmail.com>
2024-01-03 23:14:21 +0700

    Improve vite.config port x cypress port

--- part5/bloglist-frontend/cypress.config.js
+++ part5/bloglist-frontend/cypress.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from 'cypress'
+import { VITE_PORT } from './vite.config'
 
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: 'http://localhost:5173',
+    baseUrl: `http://localhost:${VITE_PORT}`,
     env: {
       RESET_API: '/api/testing/reset',
       CREATE_USER_API: '/api/users',
--- part5/bloglist-frontend/vite.config.js
+++ part5/bloglist-frontend/vite.config.js
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+export const VITE_PORT = 5179
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
+    port: VITE_PORT,
     proxy: {
       '/api': {
         target: 'http://localhost:3003',
